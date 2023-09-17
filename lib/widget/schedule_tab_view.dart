114809eb69a0f1eb6aecb4b5fe41553060f4607f
flowkater <chjw102@gmail.com>
2023-09-18 00:19:57 +0900

    fix etc

--- lib/widget/schedule_tab_view.dart
+++ lib/widget/schedule_tab_view.dart
@@ -121,7 +121,7 @@ class _DancingAnimatedImageState extends State<DancingAnimatedImage> {
   }
 
   _startImageSwitching() async {
-    while (false) {
+    while (true) {
       await Future.delayed(const Duration(milliseconds: 300));
       setState(() {
         _index = (_index + 1) % images.length;
